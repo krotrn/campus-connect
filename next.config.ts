c3665d1cffc79ed866676fe94396fd41dc71b16f
Kaushal Kumar <kaushal.cse.23@nitap.ac.in>
2025-09-07 13:16:21 +0530

    profile-part

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
     dangerouslyAllowSVG: false,
     contentDispositionType: "attachment",
