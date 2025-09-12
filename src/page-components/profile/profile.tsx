a2d80d2bd8bd273c09af3bc46960c62b280c58ae
Kaushal Kumar <kaushal.cse.23@nitap.ac.in>
2025-09-12 21:56:26 +0530

    profile-page

--- src/page-components/profile/profile.tsx
+++ src/page-components/profile/profile.tsx
@@ -1,44 +1,21 @@
-import Image from "next/image";
-import React from "react";
+"use client";
 
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Dialog from "@/page-components/profile/dialog";
+import React, { useMemo, useState } from "react";
+import { toast } from "sonner";
 
-const Profile = () => {
+import AddressCard from "@/components/profile/address-card";
+import ProfileCard from "@/components/profile/profile-card";
+import ProfileHeader from "@/components/profile/profile-header";
+import { SharedCard } from "@/components/shared/shared-card";
+
+export default function CheckoutPageComponent() {
   return (
-    <Card>
-      <CardHeader className="relative">
-        <div className="flex justify-center">
-          <Image
-            className="rounded-full"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqh7Rg8QvzgzcaQOPnbiSiRuO6D-WHYeXuLw&s"
-            width={250}
-            height={250}
-            alt="Picture of the author"
-            style={{ objectFit: "fill" }}
-          />
-          <CardAction className="absolute top-0 right-0">Owner</CardAction>
-        </div>
-      </CardHeader>
-      <CardContent className="flex justify-center flex-col items-center">
-        <p>Kaushal Kumar</p>
-        <p>kaushal.cse.23@nitap.ac.in</p>
-        <p>papum hostel, national institute of technology, AP</p>
-        <p></p>
-      </CardContent>
-      <CardFooter className="flex justify-center">
-        <Dialog />
-      </CardFooter>
-    </Card>
+    <SharedCard
+      headerContent={<ProfileHeader />}
+      contentClassName="grid lg:grid-cols-2 gap-8"
+    >
+      <ProfileCard />
+      <AddressCard />
+    </SharedCard>
   );
-};
-
-export default Profile;
+}
