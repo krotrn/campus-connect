39e29be9b8cac5bf37bfcb7b80785b9fd9b920a4
Karan Kumar Sah <krotrn.ks@gmail.com>
2025-10-10 02:41:19 +0530

    Revert "payment page added"

--- src/app/owner-shops/owner-orders/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-import OwnerOrders from "@/page-components/owner-shop/owner-orders";
-export default function page() {
-  return <OwnerOrders />;
-}
--- src/app/payment/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import QrPayment from "@/page-components/payment/qr-payment";
-
-export default function page() {
-  return <QrPayment />;
-}
