8aabb7874059b3c5c59018c273f0af61fae3f231
Karan Kumar Sah <krotrn.ks@gmail.com>
2025-12-18 21:10:00 +0530

    feat: Update Nginx configuration for improved proxy settings and add new local endpoints

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,18 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "9000",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "minio",
+        port: "9000",
+        pathname: "/**",
+      },
     ],
     dangerouslyAllowSVG: false,
     contentDispositionType: "inline",
